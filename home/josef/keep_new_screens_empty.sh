67d7eadcee5995423f4f7c82459de0f17b9e02be
Josef Schönberger <josef.schoenberger@tum.de>
2025-06-26 21:17:15 +0200

    Sway keep_new_screens_empty.sh: No longer require workspace_auto_back_and_forth

--- home/josef/keep_new_screens_empty.sh
+++ home/josef/keep_new_screens_empty.sh
@@ -6,17 +6,23 @@ pull_outputs() {
 pull_workspaces() {
 	WORKSPACES="$(swaymsg -t get_workspaces -r)"
 }
+pull_focused_workspace() {
+	FOCUSED_WORKSPACE="$(swaymsg -t get_workspaces -r | jq '.[] | select(.focused).num')"
+}
 
 pull_outputs
 pull_workspaces
+#pull_focused_workspace
 
 while true; do
 	# Wait for screen list to change
 	PREV_OUTPUTS="$OUTPUTS"
 	PREV_WORKSPACES="$WORKSPACES"
+	#PREV_FOCUSED_WORKSPACE="$FOCUSED_WORKSPACE"
 	swaymsg -t subscribe '["output","workspace"]' >/dev/null 2>&1
 	pull_outputs
 	pull_workspaces
+	pull_focused_workspace
 
 	NEW_SCREENS="$(echo "[$PREV_OUTPUTS, $OUTPUTS]" | jq '(.[1] - .[0]).[]' -r)"
 
@@ -25,7 +31,11 @@ while true; do
 		for workspace in $(echo $WORKSPACES | jq '.[] | select(.output == "'"$SCREEN"'") | .num'); do
 			PREV_OUTPUT="$(echo $PREV_WORKSPACES | jq ".[] | select(.num == $workspace) | .output" -r)"
 			echo " -> workspace on screen: $workspace (was on $PREV_OUTPUT), moving back..."
-			swaymsg "workspace number $workspace, move workspace to output $PREV_OUTPUT, workspace number $workspace" # currently requires workspace_auto_back_and_forth
+			if [ "$workspace" = "$FOCUSED_WORKSPACE" ]; then
+				swaymsg "move workspace to output $PREV_OUTPUT"
+			else
+				swaymsg "workspace number $workspace, move workspace to output $PREV_OUTPUT, workspace number $FOCUSED_WORKSPACE"
+			fi
 		done
 	done
 done
